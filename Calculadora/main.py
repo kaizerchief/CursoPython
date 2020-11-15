00432ba2d235c9b6167f31ebdab5e3be83a35772
Miguelangel <miguel.torresdev@gmail.com>
2020-11-15 18:53:02 -0300

    Avanzamos en el curso

---
+++ Calculadora/main.py
@@ -0,0 +1,14 @@
+print("************* CALCULADORA ************")
+
+# EL OBJETIVO ES HACER UNA CALCULADORA TOTALMENTE FUNCIONAL APLICANDO TODO LO QUE APRENDA EN EL CURSO
+
+# ENTRADA DE DATOS
+# TODO LO INGRESADO EN UN INPUT ES DE FORMATO STRING - MANTENER PRESENTE
+
+nombre = input("Cual es tu nombre? ")
+
+# NUMERO DE OPERACIONES
+operaciones = 0
+operaciones += 1
+
+print("Tu nombre es {nombre} ")
---
+++ Tipos-variables/tipos.py
@@ -0,0 +1,4 @@
+texto = "hola soy tu primera variable"
+numero = 22
+
+print(texto + " " + str(numero))
